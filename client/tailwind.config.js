07de1ab6503cfb1133bbb663c98cdf9d38e95cb3
Lordprettyflacko <snsolomar@gmail.com>
2023-04-22 15:20:47 -0500

    first

---
+++ client/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#c33a1b",
+
+          secondary: "#1cfdff",
+
+          accent: "#ffa86f",
+
+          neutral: "#3D4451",
+
+          "base-100": "#141526",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
