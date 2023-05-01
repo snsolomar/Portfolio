07450d7cf2e11929e4d8e8a7338a6176debd3d1f
Lordprettyflacko <snsolomar@gmail.com>
2023-05-01 00:20:37 -0500

    badge card

--- client/src/Pages/Portfolio/Portfolios.js
+++ client/src/Pages/Portfolio/Portfolios.js
@@ -36,9 +36,9 @@ const Portfolios = () => {
       title: "Weather App",
       description:
         "Weather application made using Jquery and asynchronous code. Integrated weather API to call data.",
-      stack1: "HTML",
-      stack2: "CSS",
-      stack3: "JQuery",
+      stack1: "Jquery",
+      stack2: "HTML",
+      stack3: "CSS",
 
       gitHubLink: "https://github.com/snsolomar/Weather-App",
     },
@@ -47,8 +47,8 @@ const Portfolios = () => {
       title: "Tic-Tac-Toe",
       description: "Tic-Tac-Toe web application made using React",
       stack1: "React",
-      stack2: "CSS",
-      stack3: "HTML",
+      stack2: "PostgreSQL",
+      stack3: "Express",
 
       gitHubLink: "https://github.com/snsolomar/tic-tac-toe-React-Project",
     },
@@ -58,8 +58,8 @@ const Portfolios = () => {
       description:
         "This is a web application used for grading class assignmens. I used HTML, CSS, Javascript, and React for this project. In the backend, I used PostgreSQL, Express, and Node.js",
       stack1: "React",
-      stack2: "Express",
-      stack3: "CSS",
+      stack2: "PostgreSQL",
+      stack3: "Express",
 
       gitHubLink: "www.git",
     },
