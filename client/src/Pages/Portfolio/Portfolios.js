90a8368c302b7baa34d6893811dd5cbb793b4707
Lordprettyflacko <snsolomar@gmail.com>
2023-05-01 00:51:05 -0500

    card summary update

--- client/src/Pages/Portfolio/Portfolios.js
+++ client/src/Pages/Portfolio/Portfolios.js
@@ -13,7 +13,7 @@ const Portfolios = () => {
       image: p1,
       title: "Blue Ocean",
       description:
-        "This is a web application used for grading class assignmens. I used HTML, CSS, Javascript, and React for this project. In the backend, I used PostgreSQL, Express, and Node.js",
+        "Grading web application streamlines the process of evaluating class assignments and tracking student performance. The backend is supported by PostgreSQL, Express, and Node.js, ensuring a reliable and efficient data management system. Instructors can create groups, add students, and grade assignments with ease. The application automatically calculates and updates students' average scores, which are then displayed in a visually appealing and informative graph",
       stack1: "React",
       stack2: "PostgreSQL",
       stack3: "Express",
@@ -24,7 +24,7 @@ const Portfolios = () => {
       image: p2,
       title: "Items Detail Page",
       description:
-        "This is a web application used for grading class assignmens. I used HTML, CSS, Javascript, and React for this project. In the backend, I used PostgreSQL, Express, and Node.js",
+        "hotel reservation web application serves as an item detail page, providing essential information for customers seeking accommodations. Developed using HTML, CSS, JavaScript, and React, the frontend delivers a visually engaging and user-friendly experience. The application allows users to easily browse through available rooms, view high-quality images, and read detailed descriptions, as well as check availability and pricing for their desired stay dates.",
       stack1: "React",
       stack2: "CSS",
       stack3: "Bootstrap",
@@ -35,7 +35,7 @@ const Portfolios = () => {
       image: p3,
       title: "Weather App",
       description:
-        "Weather application made using Jquery and asynchronous code. Integrated weather API to call data.",
+        "Weather application is built using jQuery and asynchronous code to deliver a seamless user experience. By integrating a robust weather API, the app retrieves real-time weather data for any entered city. Users simply input the city name and the application fetches the relevant data. his application is designed to be both lightweight and highly responsive",
       stack1: "Jquery",
       stack2: "HTML",
       stack3: "CSS",
@@ -45,7 +45,8 @@ const Portfolios = () => {
     {
       image: p4,
       title: "Tic-Tac-Toe",
-      description: "Tic-Tac-Toe web application made using React",
+      description:
+        "Tic-tac-toe web application offers users a fresh take on the classic game. One standout feature of this tic-tac-toe app is its ability to remember each move made by the user. This functionality allows players to seamlessly undo any mistakes, providing an opportunity for strategic reconsideration and improving their chances of winning.",
       stack1: "React",
       stack2: "PostgreSQL",
       stack3: "Express",
@@ -56,7 +57,7 @@ const Portfolios = () => {
       image: p5,
       title: "Trivia",
       description:
-        "This is a web application used for grading class assignmens. I used HTML, CSS, Javascript, and React for this project. In the backend, I used PostgreSQL, Express, and Node.js",
+        "Trivia web application delivers a captivating and challenging experience for players, drawing inspiration from the classic game show format. The modular nature of JSX components also makes it easy to implement design changes, resulting in a highly maintainable and scalable application. The application leverages an API to gather a diverse array of categories and questions.",
       stack1: "React",
       stack2: "PostgreSQL",
       stack3: "Express",
