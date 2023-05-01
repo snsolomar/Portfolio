7db7e33f0316d7c6ddb112b77573e339d37927e4
Lordprettyflacko <snsolomar@gmail.com>
2023-04-30 22:20:51 -0500

    portfolio image update

--- client/src/Pages/Portfolio/Portfolio.js
+++ client/src/Pages/Portfolio/Portfolio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Portfolio.css";
 import p1 from "../../Assets/Portfolio/p1.png";
+import { Link } from "react-router-dom";
 
 const Portfolio = ({ portfolio }) => {
   const { image, title, description, gitHubLink, stack1, stack2, stack3 } =
@@ -10,7 +11,9 @@ const Portfolio = ({ portfolio }) => {
     <div className="min-w-[350px] mx-10 lg:w-full portfolio-card">
       <div className="card min-w-[300px] lg:w-96 bg-base-100 shadow-xl">
         <figure>
-          <img src={image} alt="portfolio" />
+          <a href={gitHubLink} target="_blank" rel="noopener noreferrer">
+            <img src={image} alt="portfolio" />
+          </a>
         </figure>
         <div className="card-body">
           <h2 className="card-title block">
